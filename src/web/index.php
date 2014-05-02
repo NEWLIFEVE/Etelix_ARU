18e6f2f557c5c400d0dc83a799f8db2557476d3d
Manuel Zambrano <mmzmm3z@gmail.com>
2014-05-02 13:11:56 -0430

    cambio de entornos de trabajo

--- src/web/index.php
+++ src/web/index.php
@@ -1,13 +1,28 @@
 <?php
+//Definimos nuestro servidor de produccion 
+define('SERVER_NAME_PROD','aru.sacet.com.ve');
+//Obtenemos el nombre del servidor actual 
+$server=$_SERVER['SERVER_NAME'];
 
-// change the following paths if necessary
-$yii='../../../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+switch ($server)
+{
+	case SERVER_NAME_PROD:
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+		break;
+	default:
+		defined('YII_DEBUG') or define('YII_DEBUG',true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		break;
+}
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+$yii=dirname(__FILE__).'/../../../yii/framework/yii.php';
 require_once($yii);
+
+$main=require(dirname(__FILE__).'/protected/config/main.php');
+$db=require(dirname(__FILE__).'/protected/config/db.php');
+$gii=require(dirname(__FILE__).'/protected/config/gii.php');
+
+$config=CMap::mergeArray($main,$db,$gii);
+
 Yii::createWebApplication($config)->run();
