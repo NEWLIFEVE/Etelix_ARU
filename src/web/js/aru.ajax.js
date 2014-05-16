b8560c0dcf9572819d7f9033cbf92498b460ada3
Israel Tarazona <israeltarazona@gmail.com>
2014-05-16 18:52:23 -0430

    cambio de class add y remove

--- src/web/js/aru.ajax.js
+++ src/web/js/aru.ajax.js
@@ -35,12 +35,13 @@ $ARU.AJAX=(function()
             {
                  result=JSON.parse(data);
                 if (result==1){
-                $('#mensaje').removeClass("rojo");     
-                $('#mensaje').addClass("verde");    
+                $('#mensaje').removeClass("rojo");  
+                $('#mensaje').addClass("verde");           
                 $('#mensaje').html("Cambio de Contraseña Valido");
                 }
                 
                 if (result==2){
+                $('#mensaje').removeClass("verde");     
                 $('#mensaje').addClass("rojo");    
                 $('#mensaje').html("Contraseña No Valida");
                 }
