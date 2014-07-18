e96d026891c05a1e506e785218fc6a7073e6e907
Israel Tarazona <israeltarazona@gmail.com>
2014-07-18 18:26:35 -0430

    correccion para js cp

--- src/web/js/aru.ui.js
+++ src/web/js/aru.ui.js
@@ -404,9 +404,8 @@ $ARU.UI=(function(){
 
                                 lider= $ARU.AJAX.leaderExist("GET","/PositionCode/CheckLeaderExist","id_division="+id_division+ "&id_position="+id_position+"&checkLeader=true"); 
                                 dependency= $ARU.AJAX.leaderExist("GET","/PositionCode/CheckDependencyByDivision","id_division="+id_division+ "&id_position="+id_position+"&checkDivision=true"); 
-                                
-                                ExistDependency= lider= $ARU.AJAX.ExistDependency("GET","/PositionCode/CheckDependencyByDivision","id_division="+id_division+ "&id_position="+id_position +"&checkDivision=true"); 
-                                
+                              console.log(lider);
+
                                 if (lider=='false'){
                                    $('.alert-danger').html('Se Necesita Crear un Lider/Coordinador/Gerente para esta División'); 
                                    $('.alert-danger').css('display', 'block'); 
@@ -419,11 +418,7 @@ $ARU.UI=(function(){
                                    }
                                }
                                
-                              if (ExistDependency=='false'){
-                                   $('.alert-danger').html('Se Necesita Crear una Dependencia padre'); 
-                                   $('.alert-danger').css('display', 'block'); 
-                                   return false;
-                              }
+                            
                                
                                
                         }
